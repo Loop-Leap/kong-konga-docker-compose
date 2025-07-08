a7f546527ff31dc860cc3f0a36ccd667e14d2567
Nicolo Pietro Belcastro <nicolopietrobelcastro@gmail.com>
2025-07-08 21:43:46 +0200

    fix

--- kong_plugins_for_coolify/kong/plugins/jwt-user-header/schema.lua
+++ kong_plugins_for_coolify/kong/plugins/jwt-user-header/schema.lua
@@ -1,18 +1,15 @@
 -- /opt/custom_lua/kong/plugins/jwt-user-header/schema.lua
 
 local typedefs = require "kong.db.schema.typedefs"
-local PLUGIN_NAME = "jwt-user-header" -- Define plugin name
+-- Remove this line: local PLUGIN_NAME = "jwt-user-header" -- Define plugin name
 
 return {
-  name = PLUGIN_NAME,
+  name = "jwt-user-header", -- Directly use the string literal for 'name'
   fields = {
-    -- Allow this plugin to be associated with a consumer, service, or route
-    -- without requiring a configuration field for them.
-    -- This makes it a standard attachable plugin.
-    { consumer = typedefs.consumer }, -- Can be applied per consumer
-    { service = typedefs.service },   -- Can be applied per service
-    { route = typedefs.route },       -- Can be applied per route
-    { protocols = typedefs.protocols_http }, -- Can run on HTTP/HTTPS
+    { consumer = typedefs.consumer },
+    { service = typedefs.service },
+    { route = typedefs.route },
+    { protocols = typedefs.protocols_http },
     {
       config = {
         type = "record",
